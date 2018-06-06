7a8640cfb6cfa36794db3818d900541de11406f8
Phyoeieibo <phyoei083@gmail.com>
2018-06-06 18:35:20 +0630

    commit ex10 and ex11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?"),
+age=input("Your age?")
+print("How tall are you?"),
+height=input("Your height?")
+print("How much do you weight?"),
+weight=input("Your weight?")
+print("So, you're %r old, %r tall and %r heavy." % (age,height,weight))
+
+
+
